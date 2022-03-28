f1f1353084b8eca8ee1a386ddbef5e1d969f09b6
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2022-03-28 17:55:18 +0200

    get_sign() befejezese

--- image.py
+++ image.py
@@ -3,11 +3,13 @@ from os import listdir
 from os.path import join
 import numpy as np
 
+
 def contours(gray):
     _, threshold = cv2.threshold(gray, 127, 255, 0)
     contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours
+
 
 def load_signs(directory):
     signs = []
@@ -19,9 +21,15 @@ def load_signs(directory):
 
     return signs
 
+
 def get_sign(picture, signs):
     closest = 1.0
     for sign in signs:
         match = cv2.matchShapes(picture, sign, )
+        if match < closest:
+            closest = match
 
-load_signs('signs')
+    return closest
+
+
+signs = load_signs('signs')
