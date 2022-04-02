02787db8fa514a300063477520e72ae2c7b14571
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2022-04-02 18:43:21 +0200

    kepfelismero++

--- image.py
+++ image.py
@@ -1,12 +1,24 @@
 import cv2
+import numpy as np
 
+
+def display(img):
+    cv2.imshow('lol', img)
+    cv2.waitKey(0)
 
 def contours(gray):
-    _, threshold = cv2.threshold(gray, 50, 255, 0)
-    contours, hierarchy, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    median_value = int(np.median(np.unique(np.copy(gray))))
+    print(median_value)
+    _, threshold = cv2.threshold(gray, median_value, 255, 0)
+    display(threshold)
+    contours_tup, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    return contours
+    # 4.5.4 ota valamiert tuple-t ad vissza alapbol, szoval csinalunk belole listat
+    return contours_tup[0]
 
+def load_sign(file_):
+        img = cv2.imread(file_)
+        return contours(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
 
 def load_signs(file_list):
     """
@@ -14,20 +26,17 @@ def load_signs(file_list):
     """
     signs = []
     for file_ in file_list:
-        img = cv2.imread(file_)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        img = contours(img)
-        signs.append(img)
+        signs.append(load_sign(file_))
 
     return signs
 
 
 def get_sign(picture, signs):
-    closest = 1.0
+    closest = 0.0
     closest_index = None
     for index, sign in enumerate(signs):
         match = cv2.matchShapes(picture, sign, 1, 0)
-        if match < closest:
+        if match > closest:
             closest = match
             closest_index = index
 
@@ -37,5 +46,7 @@ def get_sign(picture, signs):
 
 import utils
 signs = utils.files('signs')
+print(signs)
 imgs = load_signs(signs)
-print(signs[get_sign(imgs[0], imgs)], signs)
+test = load_sign('test.jpg')
+print(signs[get_sign(test, imgs)], signs)
