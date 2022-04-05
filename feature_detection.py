76163f13a7ed689e6f3ed0134fcc6cccc8b70fe1
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2022-04-05 18:59:31 +0200

    uj modszer keposszehasonlitasra, elmeleti sikon jobb

--- feature_detection.py
+++ feature_detection.py
@@ -1,31 +1,9 @@
-import cv2
+from skimage.metrics import structural_similarity as ssim
 
-import utils
 import image
 
 
-def load_img(file_):
-    return cv2.imread(file_, 0)
-
-
 if __name__ == '__main__':
-    template_files = utils.files('signs')
-    test_files = utils.files('tests')
-    templates = [load_img(file_) for file_ in template_files]
-    tests = [load_img(file_) for file_ in test_files]
-    template_widths = [template.shape[::-1] for template in templates]
-
-    for test in tests:
-        test = test.copy()
-        for i, template in enumerate(templates):
-            try:
-                res = cv2.matchTemplate(test, template, cv2.TM_SQDIFF)
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-                cv2.rectangle(
-                        test,
-                        min_loc,
-                        (min_loc[0] + template_widths[i][0], min_loc[1] + template_widths[i][1]),
-                        255,
-                        2)
-            except:
-                pass
+    fire1 = image.load_sign('fire1.jpg')
+    fire2 = image.load_sign('fire2.jpg')
+    print(ssim(fire1, fire2))
--- image.py
+++ image.py
@@ -7,18 +7,22 @@ def display(img):
     cv2.waitKey(0)
 
 
+def match_percent(match):
+    return (1.0 - match) * 100
+
+
 def contours(gray):
     # draga mulatsag, mert csinal egy masolatot az egesz kepbol,
     # ami nagy felbontasnal bajos lehet
     median_value = int(np.median(np.unique(np.copy(gray))))
     _, threshold = cv2.threshold(gray, median_value, 255, 0)
+    threshold = cv2.resize(threshold, (1280, 720))
+    display(threshold)
     contours_tup, hierarchy = cv2.findContours(
             threshold,
             cv2.RETR_TREE,
             cv2.CHAIN_APPROX_SIMPLE)
 
-    # 4.5.4 ota valamiert tuple-t ad vissza alapbol,
-    # szoval csinalunk belole listat
     return threshold
 
 
@@ -43,7 +47,7 @@ def get_sign(picture, signs):
     closest_index = None
     for index, sign in enumerate(signs):
         match = cv2.matchShapes(picture, sign, 1, 0)
-        print(index, match)
+        print(index, match_percent(match))
         if match < closest:
             closest = match
             closest_index = index
@@ -52,15 +56,6 @@ def get_sign(picture, signs):
     # lekerhessuk melyik kephez all a legkozelebb
     return closest_index
 
+
 # ideiglenes teszt kod
 # csak akkor hivodik meg ha ezt a filet futtatjuk
-
-if __name__ == '__main__':
-    import utils
-    sign_files = utils.files('signs')
-    test_files = utils.files('tests')
-    imgs = load_signs(sign_files)
-    tests = load_signs(test_files)
-    for i, test in enumerate(tests):
-        index = get_sign(test, imgs)
-        print(f'{test_files[i]}: {index} {sign_files[index]}')
