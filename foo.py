ee72ae3d18fba62f83544f067e27ffdc6e7a04e3
Pots Odon <me@urm.om>
2022-04-12 12:33:33 +0200

    cleanup 2

--- foo.py
+++
@@ -1,8 +0,0 @@
-from adafruit_servokit import ServoKit
-import time
-kit = ServoKit(channels=16)
-kit.servo[0].set_pulse_width_range(550, 2500)
-kit.servo[0].angle = 90
-#time.sleep(1)
-#kit.servo[0].angle = 180
-
